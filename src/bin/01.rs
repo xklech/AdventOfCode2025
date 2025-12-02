d221240c1f3090324ed4a8d6a688fa451f84a679
Jaroslav Klech <jarek.klech@seznam.cz>
2025-12-02 15:33:49 +0100

    first days

---
+++ src/bin/01.rs
@@ -0,0 +1,63 @@
+use core::num;
+
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let mut position = 50;
+    let mut counter = 0;
+    for line in input.lines() {
+        let rotation = line[1..].parse::<i32>().unwrap() % 100;
+        position  += if line.starts_with('L') { -1 } else { 1 } * rotation;
+        if position < 0 {
+            position = 100 - position.abs();
+        } else if position > 99 {
+            position = position % 100;
+        }
+        if position == 0 {
+            counter += 1;
+        }
+        println!("Current position: {}", position);
+    }
+    Some(counter)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut position = 50;
+    let mut counter = 0;
+    for line in input.lines() {
+        let number = line[1..].parse::<i32>().unwrap();
+        if number >= 100 {
+            println!("Large rotation detected: {}", number);
+        }
+        let num_rotations = number / 100;
+        counter += num_rotations as u64;
+        let rotation_reminder = number % 100;
+        position  += if line.starts_with('L') { -1 } else { 1 } * rotation_reminder;
+        if position < 0 {
+            counter += if num_rotations > 0 { 0 } else { 1 };
+            position = 100 - position.abs();
+        } else if position > 99 {
+            counter += if num_rotations > 0 { 0 } else { 1 };;
+            position = position % 100;
+        }
+        println!("Current position: {}", position);
+    }
+    Some(counter)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(3));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(6));
+    }
+}
---
+++ src/bin/02.rs
@@ -0,0 +1,66 @@
+use core::str;
+
+advent_of_code::solution!(2);
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let ranges: Vec<&str> = input.split(',').collect();
+    let mut sum: u64 = 0;
+    for range in ranges {
+        let splitted: Vec<&str> = range.split('-').collect();
+        let start: u64 = splitted[0].parse().unwrap();
+        let end: u64 = splitted[1].parse().unwrap();
+        println!("Start: {}, End: {}", start, end);
+
+        for(num) in start..=end {
+            let str_num = num.to_string();
+            let halfs = str_num.split_at(str_num.len() / 2);
+            if halfs.0 == halfs.1  {
+                println!("Found same: {}", num);
+                sum += num;
+            }
+        }
+    }
+    return Some(sum)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let ranges: Vec<&str> = input.split(',').collect();
+    let mut sum: u64 = 0;
+    for range in ranges {
+        let splitted: Vec<&str> = range.split('-').collect();
+        let start: u64 = splitted[0].parse().unwrap();
+        let end: u64 = splitted[1].parse().unwrap();
+        println!("Start: {}, End: {}", start, end);
+
+        for(num) in start..=end {
+            let num_str = num.to_string();
+            for i in 1..(num_str.len()/2)+1 {
+                let part = &num_str[0..i];
+                let repeated = part.repeat(num_str.len()/i);
+                if repeated == num_str {
+                    println!("Found repeated: {}", num);
+                    sum += num;
+                    break;
+                }
+            }
+        }
+    }
+    return Some(sum)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(1227775554));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(4174379265));
+    }
+}
